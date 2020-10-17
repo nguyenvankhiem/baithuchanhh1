1655933291f8a17ed35bf96790e7b9ea710331e5
nguyenvankhiem <72650695+nguyenvankhiem@users.noreply.github.com>
2020-10-17 14:27:59 +0700

    Add files via upload

---
+++ Bai4.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bai_thuc_hanh1
+{
+    class Bai4
+    {
+        private int 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Bai_thuc_hanh1
+{
+    class Diem
+    {
+        private double x;
+        private double y;
+        public Diem()
+        {
+            x = y = 0;
+        }
+        public Diem(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void Nhap()
+        {
+            Console.WriteLine("Nhap x:");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap y:");
+            y = double.Parse(Console.ReadLine());
+        }
+        public void Hien()
+        {
+            Console.WriteLine("Toa do la {0},{1}", x, y);
+        }
+        public double Khoangcach(double a, double b)
+        {
+            return Math.Sqrt(Math.Pow(x - this.x, 2) + Math.Pow(y - this.y, 2));
+        }
+        static void Main(string[] args)
+        {
+            Diem t = new Diem();
+        }
+    }
+}
